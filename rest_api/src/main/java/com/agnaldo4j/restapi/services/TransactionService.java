3e124b69d7260862f991aef6ce0907222c2339d1
Agnaldo de Oliveira <agnaldo4j@gmail.com>
2018-04-01 22:48:15 +0200

    Refactoring

--- rest_api/src/main/java/com/agnaldo4j/restapi/services/TransactionService.java
+++ rest_api/src/main/java/com/agnaldo4j/restapi/services/TransactionService.java
@@ -14,13 +14,14 @@ import java.time.ZonedDateTime;
 @Service
 public class TransactionService {
 
+    public static final int TRANSACTION_TIME_WINDOW_SECONDS = 60;
     @Autowired
     private PersistenceAdapter persistenceAdapter;
 
     public void save(Transaction transaction) {
         try {
             ZonedDateTime now = ZonedDateTime.now( ZoneOffset.UTC );
-            long time = now.minusSeconds(60).toInstant().toEpochMilli();
+            long time = now.minusSeconds(TRANSACTION_TIME_WINDOW_SECONDS).toInstant().toEpochMilli();
             persistenceAdapter.save(transaction);
             if (transaction.timestamp() < time) throw new OldTransaction("The transaction was generated more than 60 seconds ago");
         } catch (IOException e) {
